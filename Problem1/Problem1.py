3e511f0e726a45f049c27075fd9f7cb299a65852
Sumara Ali <ali.sum@bu.edu>
2020-03-30 17:01:21 -0400

    passing all 6 test cases, accounts for URLS with subroutes

--- Problem1/Problem1.py
+++ Problem1/Problem1.py
@@ -1,3 +1,16 @@
 def URLSplit(input):
     input = input.replace(":/", "")
-    return input.split("/")
+
+    input = input.split("/") 
+
+    if len(input) > 3:
+        for i in range (2, len(input)):
+            if i == len(input) - 1:
+                input[2] = input[2] + input[i]
+            elif i == 2:
+                input[2] = input[i] + "/"
+            else:
+                input[2] = input[2] + input[i] + "/"
+
+    input = [input[0],input[1], input[2]]
+    return input
--- Problem1/Testing.py
+++ Problem1/Testing.py
@@ -14,7 +14,30 @@ class TestStringMethods(unittest.TestCase):
         actual = Problem1.URLSplit("ftp://bu.edu/")
         self.assertEqual(expected, actual)
 
+    def test3(self):
+        input = "https://www.facebook.com/messages/t/2972109159468035"
+        expected = ["https","www.facebook.com","messages/t/2972109159468035"]
+        actual = Problem1.URLSplit("https://www.facebook.com/messages/t/2972109159468035")
+        self.assertEqual(expected, actual)
     
+    def test4(self):
+        input = "https://github.com/sumara523/cs591_c2_assignment_6"
+        expected = ["https","github.com","sumara523/cs591_c2_assignment_6"]
+        actual = Problem1.URLSplit("https://github.com/sumara523/cs591_c2_assignment_6")
+        self.assertEqual(expected, actual)
+
+    def test5(self):
+        input = "https://cs-people.bu.edu/dharmesh/teaching/cs591_sp20/assignments/assignment6/"
+        expected = ["https","cs-people.bu.edu","dharmesh/teaching/cs591_sp20/assignments/assignment6/"]
+        actual = Problem1.URLSplit("https://cs-people.bu.edu/dharmesh/teaching/cs591_sp20/assignments/assignment6/")
+        self.assertEqual(expected, actual)
+
+    def test6(self):
+        input = "https://www.netflix.com/watch/70152014?trackId=200257859"
+        expected = ["https","www.netflix.com","watch/70152014?trackId=200257859"]
+        actual = Problem1.URLSplit("https://www.netflix.com/watch/70152014?trackId=200257859")
+        self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
